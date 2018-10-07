273b6967b36ac066ab846151cd254ea7f5000c9e
Urban Dzindzinovic <dzindzinovic@yahoo.com>
2018-10-07 15:00:08 +0200

    added fonts

--- index.js
+++ index.js
@@ -28,39 +28,39 @@ new Vue({
       path: window.location.pathname,
       categories: [
         { name: "Komunikativnost", isExpanded: true, tasks: [
-          "vsaj 10 osebam reci Dober dan v naslednjem tednu.",
-          "vsaj desetim osebam v naslednjem tednu ponudi pomoč.",
-          "vsaj trem ljudem, ki jih še nikoli prej nisi videl, zaželi lep dan in se jim nasmehni.",
-          "pojdi v tri ralične trgovine, v katerih še nisi bil in prosi za pomoč pri nakupu.",
-          "pokliči dva človeka v naslednjem tednu in reci, da bi se rad pogovarjal. Potem jim predlagaj temo pogovora.",
-          "nekoga, ki ga dobro poznaš, vprašaj za mnenje o stvari, ki je zate zelo pomembna"
+          { isComplete: false, text: "vsaj 10 osebam reci Dober dan v naslednjem tednu." },
+          { isComplete: false, text: "vsaj desetim osebam v naslednjem tednu ponudi pomoč." },
+          { isComplete: false, text: "vsaj trem ljudem, ki jih še nikoli prej nisi videl, zaželi lep dan in se jim nasmehni." },
+          { isComplete: false, text: "pojdi v tri ralične trgovine, v katerih še nisi bil in prosi za pomoč pri nakupu." },
+          { isComplete: false, text: "pokliči dva človeka v naslednjem tednu in reci, da bi se rad pogovarjal. Potem jim predlagaj temo pogovora." },
+          { isComplete: false, text: "nekoga, ki ga dobro poznaš, vprašaj za mnenje o stvari, ki je zate zelo pomembna" }
          ] },
-        { name: "Samozavest", isExpanded: true, tasks: [
-          "vsaj 5 minut na dan v zavetju svojega doma hodi z glavo pokonci, predstavljaj si, da na glavi nosiš vrč vode, ki ne sme pasti na tla.",
-          "Vsaj desetim osebam v enem tednu poglej v oči, pa čeprav samo za sekundo.",
-          "Vsaj trem ljudem se nasmehni in jih poglej v oči, takim, ki jih poznaš vsaj na videz.",
-          "naredi seznam vseh skrivnosti, ki jih drugi ne smejo vedeti o tebi.",
-          "naredi seznam vseh katastrof, ki bi se lahko zgodile, če bi ljudje vedeli te stvari o tebi.",
-          "izberi najmanjšo skrivnost in jo povej osebi, ki je ne poznaš.",
-          "Vsaj 5 minut hodi z visoko dvignjeno glavo med ljudmi.",
-          "Vsaj 10 ljudem v enem tednu reci dober dan in jih pri tem glej v oči.",
-          "Najdi nekoga, ki ga poznaš in mu povej svojo najlažjo skrivnost."
+        { name: "Samozavest", isExpanded: false, tasks: [
+          { isComplete: false, text: "vsaj 5 minut na dan v zavetju svojega doma hodi z glavo pokonci, predstavljaj si, da na glavi nosiš vrč vode, ki ne sme pasti na tla." },
+          { isComplete: false, text: "Vsaj desetim osebam v enem tednu poglej v oči, pa čeprav samo za sekundo." },
+          { isComplete: false, text: "Vsaj trem ljudem se nasmehni in jih poglej v oči, takim, ki jih poznaš vsaj na videz." },
+          { isComplete: false, text: "naredi seznam vseh skrivnosti, ki jih drugi ne smejo vedeti o tebi." },
+          { isComplete: false, text: "naredi seznam vseh katastrof, ki bi se lahko zgodile, če bi ljudje vedeli te stvari o tebi." },
+          { isComplete: false, text: "izberi najmanjšo skrivnost in jo povej osebi, ki je ne poznaš." },
+          { isComplete: false, text: "Vsaj 5 minut hodi z visoko dvignjeno glavo med ljudmi." },
+          { isComplete: false, text: "Vsaj 10 ljudem v enem tednu reci dober dan in jih pri tem glej v oči." },
+          { isComplete: false, text: "Najdi nekoga, ki ga poznaš in mu povej svojo najlažjo skrivnost." }
          ] },
-        { name: "Družabnost", isExpanded: true, tasks: [
-          "naredi seznam 10 aktivnosti, ki bi jih rad izkusil.",
-          "izberi eno aktivnost in pokliči prijatelja, da gre s teboj.",
-          "Izberi še eno aktivnost in povabi 2 prijatelja.",
-          "najdi aktivnost, kjer sicer ni tvojih prijateljev, a so drugi, novi ljudje.",
-          "Pri kosilu se usedi k družbi, ki jo poznaš le na videz.",
-          "vsaj 3 ljudi povabi na prijateljsko srečanje, lahko vsakega posebej."
+        { name: "Družabnost", isExpanded: false, tasks: [
+          { isComplete: false, text: "naredi seznam 10 aktivnosti, ki bi jih rad izkusil." },
+          { isComplete: false, text: "izberi eno aktivnost in pokliči prijatelja, da gre s teboj." },
+          { isComplete: false, text: "Izberi še eno aktivnost in povabi 2 prijatelja." },
+          { isComplete: false, text: "najdi aktivnost, kjer sicer ni tvojih prijateljev, a so drugi, novi ljudje." },
+          { isComplete: false, text: "Pri kosilu se usedi k družbi, ki jo poznaš le na videz." },
+          { isComplete: false, text: "vsaj 3 ljudi povabi na prijateljsko srečanje, lahko vsakega posebej." }
          ] },
-        { name: "Urejenost", isExpanded: true, tasks: [
-          "naredi seznam sprememb, ki jih želiš videti pri sebi.",
-          "Označi stvari, ki si se jim pripravljen odpovedati za to, da dosežeš bolj urejen videz.",
-          "Izberi prvo spremembo, na kateri boš začel svoj quest.",
-          "Recimo, da izbere več mišic: Vsaj 5x na teden 5 minut delaj 5 različnih vaj za...",
-          "Povečaj na 5x na teden po 10 minut",
-          "Poišči frizerja, ki bo našel pravo frizuro za tvoj izgled."
+        { name: "Urejenost", isExpanded: false, tasks: [
+          { isComplete: false, text: "naredi seznam sprememb, ki jih želiš videti pri sebi." },
+          { isComplete: false, text: "Označi stvari, ki si se jim pripravljen odpovedati za to, da dosežeš bolj urejen videz." },
+          { isComplete: false, text: "Izberi prvo spremembo, na kateri boš začel svoj quest." },
+          { isComplete: false, text: "Recimo, da izbere več mišic: Vsaj 5x na teden 5 minut delaj 5 različnih vaj za..." },
+          { isComplete: false, text: "Povečaj na 5x na teden po 10 minut" },
+          { isComplete: false, text: "Poišči frizerja, ki bo našel pravo frizuro za tvoj izgled." }
          ] },
       ]
     },
